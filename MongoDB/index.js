c2ad039ef5cdc669f3dc3394240f82b643f94ec6
nirajsakhare0033 <kangthecon0111@gmail.com>
2024-07-30 16:33:01 +0530

    express and mongodb connections completed

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import connectDB from './db/connectDB.js';
 const app = express();
 const port = process.env.PORT || 8000;
-
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017";
+connectDB(DATABASE_URL)
 app.listen(port, () => {
   console.log(`server listing on this port ${port}`)
 })
