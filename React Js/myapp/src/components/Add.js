d7914f088af1b721d3c90d4a8703dee306f4a9a3
nirajsakhare0033 <kangthecon0111@gmail.com>
2024-07-31 14:33:49 +0530

    basic of react

---
+++ React Js/myapp/src/components/Add.js
@@ -0,0 +1,8 @@
+function Add(){
+  return (
+    <div>Hello i'm Add component</div>
+  )
+}
+export default Add;
+
+
---
+++ React Js/myapp/src/components/Greatings.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Greatings = () => {
+  return (
+    <div>Greatings</div>
+  )
+}
