83296d5a7120777516a632600ff4a01a20663224
nirajsakhare0033 <kangthecon0111@gmail.com>
2024-07-31 11:55:49 +0530

    update data in data base

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -4,7 +4,7 @@ import connectDB from './db/connectDB.js';
 //single doc
 //import { singleDoc } from './models/singalDoc.js';
 //find doc with filed
-import { findDocwithField } from './models/findDocWithFiled.js';
+//import { findDocwithField } from './models/findDocWithFiled.js';
 const app = express();
 const port = process.env.PORT || 8000;
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017";
@@ -15,7 +15,7 @@ connectDB(DATABASE_URL)
 //singleDoc.js
 // singleDoc();
 //findDocWithFiled.js
-findDocwithField();
+//findDocwithField();
 
 app.listen(port, () => {
   console.log(`server listing on this port ${port}`)
---
+++ MongoDB/models/updateData.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+//default schema
+const movieSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  rating: { type: Number, required: true, min: 1, max: 10 },
+  money: {
+    type: mongoose.Decimal128,
+    required: true,
+    vaildate: (v) => (v) => 10,
+  },
+  genre: { type: Array },
+  isActive: { type: Boolean },
+  comments: [
+    { value: { type: String }, published: { type: Date, default: Date.now } },
+  ],
+});
+
+//creating models
+const movieModel = mongoose.model("Movie", movieSchema);
+const findDocwithField = async () => {
+  try {
+    const result = await movieModel.find().sort({ name: 1 });
+    console.clear();
+    console.log(result);
+    console.clear();
+  } catch (error) {
+    console.log(error);
+  }
+};
+export { findDocwithField };
