0edef0dd0b7f0f8bb304b4cf19f5dacfe70b683e
nirajsakhare0033 <kangthecon0111@gmail.com>
2024-08-07 12:13:14 +0530

    in dashboard component get all users to frontend in react

--- MERN_FULL_STACK_PROJECT/Full Stack CRUD/frontend/src/component/dashboard/dashboard.js
+++ MERN_FULL_STACK_PROJECT/Full Stack CRUD/frontend/src/component/dashboard/dashboard.js
@@ -1,10 +1,77 @@
-const dashboard = () => {
-  return (
-    <>
-      dashboard Component
-    </>
-  )
-}
+import React, { useState, useEffect } from "react";
+import { Container, Col, Row,Table,Button } from "react-bootstrap";
 
-export default dashboard
+
+const Dashboard = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/get");
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.log(
+          "error while fetching data from backend to fronted",
+          error.message
+        );
+      }
+    };
+    fetchUsers();
+
+  }, []);
+
+  return <>
+
+  <Container className="mt-5">
+    <Row>
+      <Col>
+      <h1 className="text-center">Get All Added Users In DataBase</h1>
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+               <td>
+                <Button variant="warning" onClick>
+                  Update
+                </Button>{" "}
+                <Button variant="danger" onClick>
+                  Delete
+                </Button>
+              </td> 
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      </Col>
+    </Row>
+
+  </Container>
+  
+  
+  
+  
+  
+  
+  
+  </>;
+};
+
+export default Dashboard;
+
+
+
+
 
