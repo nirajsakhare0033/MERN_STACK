ecb05b2fc91234fdc12167c3559e51bab2cb9684
nirajsakhare0033 <kangthecon0111@gmail.com>
2024-08-07 15:00:15 +0530

    update user frontend updated

--- MERN_FULL_STACK_PROJECT/Full Stack CRUD/frontend/src/component/updateUser/updateUser.js
+++ MERN_FULL_STACK_PROJECT/Full Stack CRUD/frontend/src/component/updateUser/updateUser.js
@@ -1,8 +1,105 @@
-const updateUser = () => {
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const UpdateUser = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/${id}`);
+        const data = await response.json();
+        setFormData(data);
+      } catch (error) {
+        console.log(
+          "Error while fetching data from backend to frontend",
+          error.message
+        );
+      }
+    };
+    fetchUser();
+  }, [id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:5000/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log("User updated successfully");
+        navigate("/"); 
+      } else {
+        console.error("Failed to update user");
+      }
+    } catch (error) {
+      console.error("Error while updating user", error.message);
+    }
+  };
+
   return (
     <>
-    <h1>Update User Component</h1>
+      <div className="center-form">
+        <Form onSubmit={handleSubmit}>
+          <h1>Update User</h1>
+          <Form.Group className="mb-3">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Name"
+              name="name"
+              value={formData.name || ""}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter Email"
+              name="email"
+              value={formData.email || ""}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Phone Number"
+              name="phone"
+              value={formData.phone || ""}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" size="lg">
+            Update User
+          </Button>
+        </Form>
+      </div>
     </>
-  )
-}
-export default updateUser;
+  );
+};
+
+export default UpdateUser;
